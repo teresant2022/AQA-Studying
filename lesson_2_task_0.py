205a0da295e28a7dc98020a3fb2c1128e36753ce
teresant22 <134526134+teresant2022@users.noreply.github.com>
2024-12-12 23:22:41 +0600

    Delete lesson_2_task_0.py

--- lesson_2_task_0.py
+++
@@ -1,7 +0,0 @@
-my_age = input("Введите Ваш возраст: ")  # setting the variable
-
-print("Ваш возраст: ", my_age)  # printing the 1st variable put by user
-
-my_age = (int(my_age) + 1)  # modifying the variable by adding 1 more year
-
-print("Ваш обновленный возраст: ", my_age)  # printing the updated result
