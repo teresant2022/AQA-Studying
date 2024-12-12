fd8e8a1e851ba5de564752c826bd78f2bc31478c
teresant22 <134526134+teresant2022@users.noreply.github.com>
2024-12-12 23:22:53 +0600

    Delete lesson_2_task_1.py

--- lesson_2_task_1.py
+++
@@ -1,6 +0,0 @@
-lst = ['🍇', '🍑', '🍐', '🍊', '🍌', '🍎']  # the initial list
-
-len(lst)  # getting the overall number of point in the list
-
-print(lst[0])  # printing the 1st element
-print(lst[-1])  # printin the last element
