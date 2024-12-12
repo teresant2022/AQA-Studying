9f0598ade058bd2b831f51d1fd20c7302ced3b5a
teresant22 <134526134+teresant2022@users.noreply.github.com>
2024-12-12 23:24:18 +0600

    Delete lesson_2_task_6.py

--- lesson_2_task_6.py
+++
@@ -1,5 +0,0 @@
-lst = [11, 5, 8, 32, 15, 3, 20, 132, 21, 4, 555, 9, 20]  # the initial list
-
-filtered_lst = [a for a in lst if a < 30 and a % 3 == 0]  # generating new list
-
-print(filtered_lst)  # printing the result
